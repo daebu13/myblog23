e8a1542bb10514734d002ac1b9b168d90b6035e6
daebu13 <1rn18cs040.faisal@gmail.com>
2024-02-02 15:33:13 +0530

    done pagination and comment for a particular post

--- src/main/java/com/myblog/myblog22/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.myblog.myblog22.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/myblog/myblog22/service/CommentService.java
+++
@@ -1,7 +0,0 @@
-package com.myblog.myblog22.service;
-
-import com.myblog.myblog22.payload.CommentDto;
-
-public interface CommentService {
-    CommentDto createPost(CommentDto dto, long postId);
-}
